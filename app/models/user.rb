b786c1b9719bd6f48c1f3fba2c5211c9b71d37f6
Mori116 <cacaomame096@gmail.com>
2021-05-14 06:55:57 +0000

    [Add]Top/Aboutページの追加、ログインユーザーアイコンの表示/編集機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ devise :database_authenticatable, :registerable,
        :recoverable, :rememberable, :validatable
 
 has_many :books, dependent: :destroy
+attachment :profile_image
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+get 'users/show'
 devise_for :users
 root to: 'homes#top'
+get 'homes/about'
 
 resources :books, only: [:new, :create, :index, :show, :destroy]
+resources :users, only: [:show, :edit, :update]
 
 end
