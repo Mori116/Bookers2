21ef224d9e8c1c2c4ba23867b0671b8c8cd4c850
Mori116 <cacaomame096@gmail.com>
2021-06-09 06:08:03 +0000

    [Finish]応用課題9b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,63 +1,76 @@
 class UsersController < ApplicationController
 
-def index
-  @users = User.all
-  @user = current_user
-  @book = Book.new
-end
+  def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
 
-def show
-  @user = User.find(params[:id])
-  @currentUserEntry = Entry.where(user_id: current_user.id)
-  @userEntry = Entry.where(user_id: @user.id)
-    unless @user.id == current_user.id
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id then
-            @isRoom = true
-            @roomId = cu.room_id
+  def show
+    @user = User.find(params[:id])
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+      unless @user.id == current_user.id
+        @currentUserEntry.each do |cu|
+          @userEntry.each do |u|
+            if cu.room_id == u.room_id then
+              @isRoom = true
+              @roomId = cu.room_id
+            end
           end
         end
+        if @isRoom
+        else
+          @room = Room.new
+          @entry = Entry.new
+        end
       end
-      if @isRoom
-      else
-        @room = Room.new
-        @entry = Entry.new
-      end
-    end
 
-    @book = Book.new
+      @book = Book.new
+      @books = @user.books
+      @today_book =  @books.created_today
+      @yesterday_book = @books.created_yesterday
+      @this_week_book = @books.created_this_week
+      @last_week_book = @books.created_last_week
+      # 指定期間の投稿数
+
+  end
+
+  def search
+    @user = User.find(params[:user_id])
     @books = @user.books
-    @today_book =  @books.created_today
-    @yesterday_book = @books.created_yesterday
-    @this_week_book = @books.created_this_week
-    @last_week_book = @books.created_last_week
-    # 投稿数
-
-end
-
-def edit
-  @user = User.find(params[:id])
-  if @user != current_user
-    redirect_to user_path(current_user)
+    @book = Book.new
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
+    end
+    # 指定日の投稿数の検索
   end
-end
 
-def update
-  @user = User.find(params[:id])
-  if @user.update(user_params)
-    flash[:notice] = 'You have updated user successfully.'
-    redirect_to user_path(@user.id)
-  else
-    @users = User.all
-    render :edit
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
-end
 
-private
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'You have updated user successfully.'
+      redirect_to user_path(@user.id)
+    else
+      @users = User.all
+      render :edit
+    end
+  end
 
-def user_params
-  params.require(:user).permit(:name, :profile_image, :introduction)
-end
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,12 @@ resources :users, only: [:index, :show, :edit, :update] do
   resource :relationships, only: [:create, :destroy]
   get 'followings' => 'relationships#followings', as: 'followings'
   get 'followers' => 'relationships#followers', as: 'followers'
+  get "search", to: "users#search"
+  # 各ユーザーの投稿数
 end
 
 get '/search', to: 'search#search'
+# Book,Userの検索
 
 resources :messages, only: [:create]
 resources :rooms, only: [:create, :show]
