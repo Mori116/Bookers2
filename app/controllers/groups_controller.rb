406e4cd15b2824f4e04f3abc4417599e11a468b0
Mori116 <cacaomame096@gmail.com>
2021-06-09 11:40:54 +0000

    [Finish]応用課題7c

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,17 +6,31 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.owner_id = current_user.id
     @group.save
     redirect_to groups_path
   end
 
   def index
+    @groups = Group.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
+    @group = Group.find(params[:id])
+    @user = current_user
+    @book = Book.new
   end
 
   def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to group_path(@group)
   end
 
   private
